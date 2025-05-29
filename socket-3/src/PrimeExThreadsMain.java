d1d429f14aa22ae71ae420e20d0c526c3aa1c508
TamegaiShuta <shu_tamegai@outlook.jp>
2025-05-29 22:05:49 +0900

    Prime

---
+++ socket-3/src/PrimeExThreadsMain.java
@@ -0,0 +1,15 @@
+public class PrimeExThreadsMain extends Thread{
+    int start;
+    int end;
+    boolean[] isPrime;
+
+    public PrimeExThreadsMain(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public void run() {
+        
+    }
+
+}
